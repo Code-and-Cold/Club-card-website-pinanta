f5a286b99189ad85da7b3e077164cf9f664735e8
Kole565 <rjkz565rjkz565@gmail.com>
2026-07-19 19:40:20 +0300

    feat(frontend): initial Vue project with Vite (for SPA without SSR)

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+
+<template>
+  <head><title>Club Frontend Prototype</title></head>
+
+  <body>
+
+    <div class="test">
+       <h1>Здесь будет наш свёрстанный сайт, нужно только подождать...</h1>
+    </div>
+
+  </body>
+
+</template>
+
+<style scoped>
+
+  * {
+    padding: 0;
+    margin: 0;
+  }
+
+  h1 {
+    text-align: center;    
+    color: white;
+  }
+
+  div.test {
+    background-color: #002A4C;
+    width: 315px;
+    height: 300px;
+  }
+
+</style>
