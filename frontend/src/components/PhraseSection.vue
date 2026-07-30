384e4875a731b7cf18c563e65b004393a6af0240
Kole565 <rjkz565rjkz565@gmail.com>
2026-07-30 12:25:15 +0300

    feat(frontend): PhraseSection

--- frontend/src/components/PhraseSection.vue
+++ frontend/src/components/PhraseSection.vue
@@ -1,33 +1,52 @@
 <template>
-  <section class="phrase">
-    <p class="phrase__text">
-      Хватит писать учебные лабы «в стол». <mark class="phrase__mark">Присоединяйся к нам</mark>,
+  <section class="phrase" data-testid="phrase-section">
+    <span class="phrase__text" data-testid="phrase-section__phrase">
+      Хватит писать учебные лабы «в стол».
+      <mark class="phrase__mark" data-testid="phrase-section__highlight">Присоединяйся к нам</mark>,
       чтобы делать реальные проекты в кайфовой компании и прокачать резюме еще до выпуска.
-    </p>
+    </span>
   </section>
 </template>
 <script setup></script>
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
 .phrase {
   margin: 0;
-  padding: 0 30px;
+  padding: 0 20px;
   display: flex;
   background-color: #002f55;
-  min-height: clamp(470px, 2.5vw, 100vh);
+  min-height: clamp(470px, 100vw, 900px);
   align-items: center;
   justify-content: center;
 }
 
 .phrase__text {
+  width: 100%;
+  max-width: 1293px;
+  min-height: 224px;
+
   color: white;
-  font-size: clamp(25px, 2.5vw, 60px);
-  max-width: 1290px;
-  font-weight: 300;
-  min-height: 225px;
+  font-size: 25px;
+  font-weight: 400;
+  line-height: 110%;
 }
 
 .phrase__mark {
   color: #3bb0e3;
   background-color: rgba(0, 0, 0, 0);
+}
+
+@media (min-width: 1024px) {
+  .phrase {
+    padding: 285px 73px 265px 74px;
+    min-height: 900px;
+  }
+  .phrase__text {
+    font-size: 60px;
+    line-height: 117%;
+  }
 }
 </style>
---
+++ frontend/test/PhraseSection.spec.ts
@@ -0,0 +1,120 @@
+import { describe, expect, test } from 'vitest'
+import { render } from 'vitest-browser-vue'
+import { page } from 'vitest/browser'
+
+import PhraseSection from '../src/components/PhraseSection.vue'
+
+import '../src/assets/styles/global.css'
+
+describe('PhraseSection', () => {
+  test('mobile viewport rendering (375px)', async () => {
+    await page.viewport(375, 500) // Высота в макете - 470, запас для проверки ограничения по высоте
+
+    const { getByTestId } = render(PhraseSection)
+
+    // Контейнер секции
+    const section = getByTestId('phrase-section')
+    await expect.element(section).toBeInTheDocument()
+    await expect.element(section).toBeVisible()
+    await expect.element(section).toHaveStyle({
+      backgroundColor: '#002F55',
+      width: '375px',
+      height: '470px',
+      paddingLeft: '20px',
+      paddingRight: '20px',
+    })
+
+    // Фраза
+    const phrase = getByTestId('phrase-section__phrase')
+    await expect.element(phrase).toBeInTheDocument()
+    await expect.element(phrase).toBeVisible()
+    await expect
+      .element(phrase)
+      .toHaveTextContent(
+        'Хватит писать учебные лабы «в стол». Присоединяйся к нам, чтобы делать реальные проекты в кайфовой компании и прокачать резюме еще до выпуска.',
+      )
+    await expect.element(phrase).toHaveStyle({
+      width: '335px',
+      height: '224px',
+
+      fontFamily: 'JetBrains Mono',
+      fontSize: '25px',
+      fontWeight: '400',
+      color: '#FFFFFF',
+      lineHeight: '110%',
+    })
+
+    // Выделение
+    const highlightedText = getByTestId('phrase-section__highlight')
+    await expect.element(highlightedText).toBeInTheDocument()
+    await expect.element(highlightedText).toHaveTextContent('Присоединяйся к нам')
+    await expect.element(highlightedText).toHaveStyle({
+      color: '#3BB0E3',
+    })
+  })
+
+  test('все обязательные элементы присутствуют и имеют правильную структуру', async () => {
+    const { getByTestId } = render(PhraseSection)
+
+    await expect.element(getByTestId('phrase-section')).toBeInTheDocument()
+    await expect.element(getByTestId('phrase-section')).toBeVisible()
+    await expect.element(getByTestId('phrase-section__phrase')).toBeInTheDocument()
+    await expect.element(getByTestId('phrase-section__phrase')).toBeVisible()
+  })
+})
+
+describe('PhraseSectionDesktop', () => {
+  test('desktop viewport rendering (1440px)', async () => {
+    await page.viewport(1440, 1024) // Высота в макете - 900, запас для проверки ограничения по высоте
+
+    const { getByTestId } = render(PhraseSection)
+
+    // Контейнер секции
+    const section = getByTestId('phrase-section')
+    await expect.element(section).toBeInTheDocument()
+    await expect.element(section).toBeVisible()
+    await expect.element(section).toHaveStyle({
+      backgroundColor: '#002F55',
+      width: '1440px',
+      height: '901.016px',
+      padding: '285px 73px 265px 74px', // С какого-то перепугу в макете figma на desktop статичный текст без адаптива
+    })
+
+    // Фраза
+    const phrase = getByTestId('phrase-section__phrase')
+    await expect.element(phrase).toBeInTheDocument()
+    await expect.element(phrase).toBeVisible()
+    await expect
+      .element(phrase)
+      .toHaveTextContent(
+        'Хватит писать учебные лабы «в стол». Присоединяйся к нам, чтобы делать реальные проекты в кайфовой компании и прокачать резюме еще до выпуска.',
+      )
+    await expect.element(phrase).toHaveStyle({
+      width: '1293px',
+      height: '351.016px',
+
+      fontFamily: 'JetBrains Mono',
+      fontSize: '60px',
+      fontWeight: '400',
+      color: '#FFFFFF',
+      lineHeight: '117%',
+    })
+
+    // Выделение
+    const highlightedText = getByTestId('phrase-section__highlight')
+    await expect.element(highlightedText).toBeInTheDocument()
+    await expect.element(highlightedText).toHaveTextContent('Присоединяйся к нам')
+    await expect.element(highlightedText).toHaveStyle({
+      color: '#3BB0E3',
+    }) // TODO: Refactor those two big tests to be one with loop
+  })
+
+  test('все обязательные элементы присутствуют и имеют правильную структуру', async () => {
+    const { getByTestId } = render(PhraseSection)
+
+    await expect.element(getByTestId('phrase-section')).toBeInTheDocument()
+    await expect.element(getByTestId('phrase-section')).toBeVisible()
+    await expect.element(getByTestId('phrase-section__phrase')).toBeInTheDocument()
+    await expect.element(getByTestId('phrase-section__phrase')).toBeVisible()
+  })
+})
