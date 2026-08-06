548cd8f3c4879d5609b111e5a7ee6ec2add97412
Kole565 <rjkz565rjkz565@gmail.com>
2026-08-06 10:37:37 +0300

    chore(ci): lint & format

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,31 +11,31 @@ import RecentEvents from './components/RecentEvents.vue'
 const team = [
   {
     id: 1,
-    photo:  new URL('./assets/images/cheremsha.png', import.meta.url).href,
+    photo: new URL('./assets/images/cheremsha.png', import.meta.url).href,
     name: 'За Лупкин',
     position: 'Директор',
   },
   {
     id: 2,
-    photo:  new URL('./assets/images/cheremsha.png', import.meta.url).href,
+    photo: new URL('./assets/images/cheremsha.png', import.meta.url).href,
     name: 'Яша Лава',
     position: 'Уже уволен',
   },
   {
     id: 3,
-    photo:  new URL('./assets/images/cheremsha.png', import.meta.url).href,
+    photo: new URL('./assets/images/cheremsha.png', import.meta.url).href,
     name: 'Спайдер мент',
     position: 'Офицер',
   },
   {
     id: 4,
-    photo:  new URL('./assets/images/cheremsha.png', import.meta.url).href,
+    photo: new URL('./assets/images/cheremsha.png', import.meta.url).href,
     name: 'Пупу Пу',
     position: 'ОАО',
   },
   {
     id: 5,
-    photo:  new URL('./assets/images/cheremsha.png', import.meta.url).href,
+    photo: new URL('./assets/images/cheremsha.png', import.meta.url).href,
     name: 'ААААААААААААААА',
     position: 'ААААААААА',
   },
@@ -44,7 +44,7 @@ const team = [
 const recentEvents = [
   {
     id: 1,
-    photo:  new URL('./assets/images/cheremsha.png', import.meta.url).href,
+    photo: new URL('./assets/images/cheremsha.png', import.meta.url).href,
     title: 'Проекты, которыми пользуются',
     text: 'Мы не делаем калькуляторы и todo-моды. Мы берём в работу живые проекты, решаем настоящие проблемы и доводим продукт до релиза.',
     fullText:
@@ -53,21 +53,21 @@ const recentEvents = [
   },
   {
     id: 2,
-    photo:  new URL('./assets/images/cheremsha.png', import.meta.url).href,
+    photo: new URL('./assets/images/cheremsha.png', import.meta.url).href,
     title: 'Проекты, которыми пользуются',
     text: 'Мы не делаем калькуляторы и todo-моды. Мы берём в работу живые проекты, решаем настоящие проблемы и доводим продукт до релиза.',
     data: '23.07.2026',
   },
   {
     id: 3,
-    photo:  new URL('./assets/images/cheremsha.png', import.meta.url).href,
+    photo: new URL('./assets/images/cheremsha.png', import.meta.url).href,
     title: 'Проекты, которыми пользуются',
     text: 'Мы не делаем калькуляторы и todo-моды. Мы берём в работу живые проекты, решаем настоящие проблемы и доводим продукт до релиза.',
     data: '23.07.2026',
   },
   {
     id: 4,
-    photo:  new URL('./assets/images/cheremsha.png', import.meta.url).href,
+    photo: new URL('./assets/images/cheremsha.png', import.meta.url).href,
     title: 'Проекты, которыми пользуются',
     text: 'Мы не делаем калькуляторы и todo-моды. Мы берём в работу живые проекты, решаем настоящие проблемы и доводим продукт до релиза.',
     data: '23.07.2026',
