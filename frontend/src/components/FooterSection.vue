b67101c0975afd1dccf61d289cf9bad7c377510f
Kole565 <rjkz565rjkz565@gmail.com>
2026-08-11 20:19:10 +0300

    feat(frontend, footer): logos and better reconciliation with figma

--- frontend/src/components/FooterSection.vue
+++ frontend/src/components/FooterSection.vue
@@ -3,7 +3,10 @@
 <template>
   <section class="section footer">
     <div class="footer__row footer__top">
-      <p>ЛОГО САФУ И КЛУБА</p>
+      <div class="footer__icons-wrapper">
+        <img class="footer__club-icon" src="../assets/vector/club_logo.svg" alt="Лого Клуба" />
+        <img class="footer__narfu-icon" src="../assets/vector/narfu_logo.svg" alt="Лого САФУ" />
+      </div>
       <img class="footer__vk-icon" src="../assets/vector/ri_vk-fill.svg" alt="Группа в ВК" />
     </div>
     <hr class="footer__divider" />
@@ -29,6 +32,8 @@
   justify-content: space-between;
   align-items: center;
   gap: 20px;
+
+  padding-bottom: 55px !important; /* FIXME: Соответствие макету, несоответствие инструкциям в беседе */
 }
 
 .footer__row {
@@ -42,12 +47,20 @@
 }
 
 .footer__top {
-  /* Здесь в макете шрифт, отличный от title, subtitle, text */
-  font-family: Inter;
-  font-size: clamp(25px, calc((100vw - 375px) / (1440px - 375px) * (35px - 25px) + 25px), 35px);
-  font-weight: 400; /* Regular */
-  line-height: 1.05;
-  color: white;
+}
+
+.footer__icons-wrapper {
+  width: clamp(
+    150px,
+    calc((100vw - 375px) / (1440px - 375px) * (364px - 150px) + 150px),
+    364px
+  ); /* FIXME: 150px взято случайно, в макете нет mobile иконок */
+  height: fit-content;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .footer__divider {
@@ -63,6 +76,22 @@
   .footer__bottom {
     flex-direction: row-reverse;
   }
+}
+
+.footer__club-icon {
+  width: clamp(
+    150px,
+    calc((100vw - 375px) / (1440px - 375px) * (272px - 150px) + 150px),
+    272px
+  ); /* FIXME: 150px взято случайно, в макете нет mobile иконок */
+}
+
+.footer__narfu-icon {
+  width: clamp(
+    50px,
+    calc((100vw - 375px) / (1440px - 375px) * (82px - 50px) + 50px),
+    82px
+  ); /* FIXME: 50px взято случайно, в макете нет mobile иконок */
 }
 
 .footer__vk-icon {
