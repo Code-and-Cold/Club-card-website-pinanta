9ffb33f0dd238f069f238e8c8f894df185644d5c
Kole565 <rjkz565rjkz565@gmail.com>
2026-08-24 14:00:35 +0300

    feat(api): get site data call, for frontend logic (ommited here due to interfering with gh-pages deploy) see feat/integration/get-and-apply-example

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,5 @@
 const API_BASE_URL = '/api'
+// const API_BASE_URL = 'http://195.133.11.12:8081/api' // Для проверки загрузки из публичного api, лучше использовать вариант с /api с docker-compose развертыванием
 
 export const api = {
   async submitFeedback(data) {
@@ -8,6 +9,21 @@ export const api = {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    return response.json()
+  },
+
+  async getSiteData() {
+    const response = await fetch(`${API_BASE_URL}/site`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
 
     if (!response.ok) {
